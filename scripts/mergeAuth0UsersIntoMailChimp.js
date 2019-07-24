353dd6cde8b5bd669c3c3b2be3031a458be2dc27
Meta Craft <github@meta-craft.com>
2019-07-24 17:40:36 +0100

    Update mergeAuth0UsersIntoMailChimp.js

--- scripts/mergeAuth0UsersIntoMailChimp.js
+++ scripts/mergeAuth0UsersIntoMailChimp.js
@@ -1,17 +1,124 @@
 'use strict';
 
+const Q = require("q");
+
+const subscribe = (mailchimp, listId, users) => {
+
+  const deferred = Q.defer();
+
+  var list = {
+    id: listId,
+    batch: [],
+    double_optin: false,
+    update_existing: true,
+    replace_interests: true
+  };
+
+  var size = 2000, total = users.length, processed = 0, count = 1;
+  var total_pages = parseInt((users.length / size), 10) + 1;
+
+  console.log('AME: Beginning Mailchimp subscription (' + total_pages + ' total calls required)');
+
+  var synchronizeToMailchimp = function(start) {
+    list.batch = users.splice(start, size);
+
+    console.log('AME: Subscribing to Mailchimp, please wait (' + count + '/' + total_pages + ') ...');
+    mailchimp.lists_batch_subscribe(
+      list,
+      function (err, res) {
+        if (err) {          
+          return deferred.reject(err)          
+        }
+
+        processed += size;
+
+        console.log('AME: Mailchimp subscribe batch completed successfully; processed ' + (processed < total ? processed : total) + '/' + total + ' records');
+
+        if (res.error_count > 0) {
+          console.log('AME: ' + res.error_count + ' error(s) encountered from Mailchimp:' + JSON.stringify(res.errors));
+        }
+
+        count++;
+        if (processed < total) {
+            synchronizeToMailchimp(processed);
+        } else {
+            return deferred.resolve()            
+        }
+      }
+    );
+  };
+
+  synchronizeToMailchimp(0);
+
+  return deferred.promise;
+}
+
+const unsubscribe = (mailchimp, listId, users) => {
+
+  const deferred = Q.defer();
+  
+  var list = {
+    id: listId,
+    batch: [],
+    delete_member: true    
+  };
+
+  var size = 2000, total = users.length, processed = 0, count = 1;
+  var total_pages = parseInt((users.length / size), 10) + 1;
+
+  console.log('AME: Beginning Mailchimp unsubscription (' + total_pages + ' total calls required)');
+
+  var synchronizeToMailchimp = function(start) {
+    list.batch = users.splice(start, size);
+
+    console.log(list.batch)
+    console.log('AME: Unsubscribing from Mailchimp, please wait (' + count + '/' + total_pages + ') ...');
+
+    mailchimp.lists_batch_unsubscribe(
+      list,
+      function (err, res) {
+        if (err) {          
+          return deferred.reject(err)          
+        }
+
+        processed += size;
+
+        console.log('AME: Mailchimp unsubscribe batch completed successfully; processed ' + (processed < total ? processed : total) + '/' + total + ' records');
+
+        if (res.error_count > 0) {
+          console.log('AME: ' + res.error_count + ' error(s) encountered from Mailchimp:' + JSON.stringify(res.errors));
+        }
+
+        count++;
+        if (processed < total) {
+            synchronizeToMailchimp(processed);
+        } else {
+            return deferred.resolve()            
+        }
+      }
+    );
+  };
+
+  synchronizeToMailchimp(0);
+
+  return deferred.promise;
+}
+
 var mergeAuth0UsersIntoMailChimp = function (config, mailchimp) {
   return function (context, callback) {
 
     // Upload users (add new or update existing ones)
-    var listId = context.mailChimpList.id;
-    var users = context.users;
+    const listId = context.mailChimpList.id;
+    const users = context.users;
+    const mailChimpUsers = context.mailChimpUsers;
 
     console.log('AME: ' + users.length + ' users retrieved from Auth0');
 
-    var complete = users.filter(function (user) {
+    const filteredUsers = users.filter(function (user) {
       return !user.email.includes('+');
-    }).map(function (user) {
+    })
+
+    const mailChimpUsersToSubscribe = filteredUsers.map(function (user) {
       return {
         email: {
           email: user.email
@@ -23,53 +130,23 @@ var mergeAuth0UsersIntoMailChimp = function (config, mailchimp) {
         }
       };
     });
+        
+    const filteredUsersEmails = filteredUsers.map(u => u.email)
+    const mailChimpUsersToDelete = mailChimpUsers.filter(e => !filteredUsersEmails.includes(e))
 
-    console.log('AME: ' + complete.length + ' valid email address users to synchronize with Mailchimp');
+    console.log('AME: ' + mailChimpUsersToDelete.length + ' MailChimp email addresses to remove');
+    console.log('AME: ' + mailChimpUsersToSubscribe.length + ' valid email address users to synchronize with Mailchimp');
 
-    var list = {
-      id: listId,
-      batch: [],
-      double_optin: false,
-      update_existing: true,
-      replace_interests: true
-    };
-
-    var size = 2000, total = complete.length, processed = 0, count = 1;
-    var total_pages = parseInt((complete.length / size), 10) + 1;
-
-    console.log('AME: Beginning Mailchimp synchronization (' + total_pages + ' total calls required)');
-
-    var synchronizeToMailchimp = function(start) {
-        list.batch = complete.splice(start, size);
-
-        console.log('AME: Synchronizing with Mailchimp, please wait (' + count + '/' + total_pages + ') ...');
-        mailchimp.lists_batch_subscribe(
-          list,
-          function (err, res) {
-            if (err) {
-              console.error('AME: ' + err);
-              return callback(err);
-            }
-
-            processed += size;
-
-            console.log('AME: Mailchimp batch list update completed successfully; processed ' + (processed < total ? processed : total) + '/' + total + ' records');
-
-            if (res.error_count > 0) {
-              console.log('AME: ' + res.error_count + ' error(s) encountered from Mailchimp:' + JSON.stringify(res.errors));
-            }
-
-            count++;
-            if (processed < total) {
-                synchronizeToMailchimp(processed);
-            } else {
-                return callback(null, context)
-            }
-          }
-        );
-    };
-
-    synchronizeToMailchimp(0);
+    Promise.all([
+      unsubscribe(mailchimp, listId, mailChimpUsersToDelete.map(e => ({ email: e }))),
+      subscribe(mailchimp, listId, mailChimpUsersToSubscribe)
+    ]).then(_ => {
+      callback(null, context)
+    })
+    .catch(err => {
+      console.error('AME: ' + err);
+      callback(err)    
+    })
   };
 };
 
